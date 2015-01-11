975b36a448338493fa607622b14875eb6c5fb6fa
Matt Linaberry <matt.linaberry@we-ics.com>
2015-01-11 11:33:57 -0500

    Accessing Views in Code
    Complete Postcard thru the Accessing Views in Code lesson!

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -13,9 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextfield: UITextField!
     @IBOutlet weak var enterMessageTextfield: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
     
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         messageLabel.hidden = false
+        messageLabel.text = enterMessageTextfield.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        enterMessageTextfield.text = ""  // make me empty
+        enterMessageTextfield.resignFirstResponder()  //remove the keyboard.
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
     
     override func viewDidLoad() {
